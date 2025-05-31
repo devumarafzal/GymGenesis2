875cb560f997f72e26148d4767eb11fd52a6437c
22-NTU-CS-1212 Muhammad Umar <mumarafzal2001@gmail.com>
2025-05-31 21:55:05 +0000

    remove the "Our Premier Services & Classes" section from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import HeroSection from "@/components/sections/HeroSection";
-import ServicesSection from "@/components/sections/ServicesSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
 import LeadCaptureSection from "@/components/sections/LeadCaptureSection";
 import ContactSection from "@/components/sections/ContactSection";
@@ -12,7 +11,6 @@ export default function HomePage() {
       <Navbar />
       <main className="flex-grow">
         <HeroSection />
-        <ServicesSection />
         <TestimonialsSection />
         <LeadCaptureSection />
         <ContactSection />
